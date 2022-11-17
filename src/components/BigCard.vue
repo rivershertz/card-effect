0c0641bb42614b2620cc839e8b2360efb3166cad
rivershertz <river16@shertz.com>
2022-11-17 22:06:26 +0200

    big cards stacked

---
+++ src/components/BigCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="h-60 w-44 absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-2xl"
+    :class="styles"
+  ></div>
+</template>
+<script setup>
+  const props = defineProps({
+    styles: String,
+  });
+</script>
---
+++ src/components/CardStack.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-[40vw] h-[50%] relative border">
+    <BigCard styles="rotate-3 bg-violet-300" />
+    <SmallCard />
+    <BigCard styles="-rotate-3 bg-violet-400" />
+    <SmallCard />
+    <BigCard styles="-rotate-6 bg-violet-600" />
+    <SmallCard />
+    <BigCard styles="rotate-7 bg-violet-800" />
+    <SmallCard />
+  </div>
+</template>
+<script setup>
+  import SmallCard from './SmallCard.vue';
+  import BigCard from './BigCard.vue';
+</script>
